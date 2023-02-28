93fb7824cd38c11c371dba7f30e5ffcebcf6e342
Yaroslav <yaroslav.nogovitsyn@yandex.ru>
2023-02-28 13:28:22 +0300

    add sqlite database

--- main.py
+++ main.py
@@ -1,7 +1,44 @@
-from flask import Flask, render_template, request, flash, session, redirect, url_for, abort
+import sqlite3
+import os
+from flask import Flask, render_template, request, flash, session, redirect, url_for, abort, g
+
+# конфигурация
+DATABASE = '/tmp/flsite.db'
+DEBUG = True
+SECRET_KEY = 'fdgfh78@#5?>gfhf89dx,v06k'
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'dadsadasdzxcdf19'
+app.config.from_object(__name__)
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db')))
+
+
+def connect_db():
+    conn = sqlite3.connect(app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row  # будут возвращаться не кортежи, а словари
+    return conn
+
+
+def create_db():
+    """Вспомогательная функция для создания таблиц БД"""
+    db = connect_db()
+    with app.open_resource('sq_db.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+
+
+def get_db():
+    """Соединение с БД, если оно еще не установлено"""
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
+
 
 menu = [{"name": "Установка", "url": "install-flask"},
         {"name": "Первое приложение", "url": "first-app"},
@@ -10,6 +47,7 @@ menu = [{"name": "Установка", "url": "install-flask"},
 
 @app.route('/')
 def index():
+    db = get_db()
     return render_template('index.html', menu=menu)
 
 
