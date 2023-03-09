fed54fe2935e24bee6c32c9b41202d958c8638a4
Yaroslav <yaroslav.nogovitsyn@yandex.ru>
2023-03-09 16:46:36 +0300

    added the ability to add articles with ready-made html

--- sq_db.sql
+++ sq_db.sql
@@ -5,9 +5,10 @@ CREATE TABLE IF NOT EXISTS mainmenu(
 );
 
 CREATE TABLE IF NOT EXISTS posts (
-id integer PRIMARY KEY AUTOINCREMENT,
-title text NOT NULL,
-text text NOT NULL,
-time integer NOT NULL
+    id integer PRIMARY KEY AUTOINCREMENT,
+    title text NOT NULL,
+    text text NOT NULL,
+    url text NOT NULL,
+    time integer NOT NULL
 );
 
